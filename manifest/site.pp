d81d319cbaf0992b9f43fd11eddf87e9a3a75f1d
C Martin <fxgx99@gmail.com>
2020-06-03 11:26:00 -0400

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,12 +1,12 @@
 node default {
-file {'/root/README':
-  ensure => file,
-  content => 'This is a readme',
-  owner => 'root',    
-  }
 }
 node 'master.puppet.vm'{
  include role::master_server
+ file {'/root/README':
+  ensure => file,
+  content => '$fqdn',
+  owner => 'root',    
+  }
  }
 
 node /^web/ {
